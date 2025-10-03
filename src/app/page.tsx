88c641e31e101b8fa90e8c2bf5ba0da7efeb138b
Muhammad Hafizh Zhafran <hazhranz@gmail.com>
2025-10-03 13:39:20 +0000

    image yang di hero section terlalu gelap, ubah menjadi warna yang lebih

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
             src="https://images.unsplash.com/photo-1553356084-58ef4a67b2a7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Abstract blue water pattern"
             fill
-            className="object-cover opacity-20"
+            className="object-cover opacity-40"
             data-ai-hint="abstract blue water"
             priority
           />
