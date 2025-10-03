57d42aacc3f823b562477134d07189797d007105
Muhammad Hafizh Zhafran <hazhranz@gmail.com>
2025-10-03 14:24:16 +0000

    bautkan i framenya scrolling dari ujung kiri ke ujung kanan, dengan meng

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -800,17 +800,34 @@ export default function Home() {
         <p className="font-semibold text-muted-foreground">
           {homeDict.supportedBy}
         </p>
-        <div style={{position: 'relative', width: '100%', height: 0, paddingTop: '6.2500%', paddingBottom: 0, boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)', marginTop: '1.6em', marginBottom: '0.9em', overflow: 'hidden', borderRadius: '8px', willChange: 'transform'}}>
-          <iframe loading="lazy" style={{position: 'absolute', width: '100%', height: '100%', top: 0, left: 0, border: 'none', padding: 0, margin: 0}} src="https://www.canva.com/design/DAG0vgSu8Q4/J8dlQa7zH4MCir-aF4d54g/view?embed" allowFullScreen={true} allow="fullscreen">
-          </iframe>
-        </div>
+        <LogoLoop>
+          <div
+            style={{
+              position: 'relative',
+              width: '1200px',
+              height: '75px',
+              pointerEvents: 'none',
+            }}
+          >
+            <iframe
+              loading="lazy"
+              style={{
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                top: 0,
+                left: 0,
+                border: 'none',
+                padding: 0,
+                margin: 0,
+              }}
+              src="https://www.canva.com/design/DAG0vgSu8Q4/J8dlQa7zH4MCir-aF4d54g/view?embed"
+              allowFullScreen={true}
+              allow="fullscreen"
+            ></iframe>
+          </div>
+        </LogoLoop>
       </MotionWrapper>
     </div>
   );
 }
-
-    
-
-    
-
-    
