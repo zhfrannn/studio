2f860dad7f7c15289ae424a85999fc1b991b5723
Muhammad Nabil Azra <nabilazra1234@gmail.com>
2025-10-10 06:41:16 +0000

    GAGAL. IMPORT ALAN SANS : Embed code in the <head> of your html
    <style>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,18 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Alan+Sans:wght@300..900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+      <head />
       <body className="font-body antialiased bg-background text-foreground">
         <ThemeProvider
           attribute="class"
