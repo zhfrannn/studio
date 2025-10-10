f3c148e357268d45b54bcb54b19efdfbfc0f2d12
Muhammad Nabil Azra <nabilazra1234@gmail.com>
2025-10-10 08:47:14 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@g

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,14 +1,11 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {Dotprompt} from '@genkit-ai/dotprompt';
-import {AIMiddleware} from 'genkit';
 
 export const ai = genkit({
   plugins: [
     googleAI({
       apiVersion: 'v1beta',
     }),
-    new Dotprompt(),
   ],
   model: 'gemini-1.5-flash',
 });
