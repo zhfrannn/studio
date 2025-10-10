118283aa5b701ac51e089504ef160124b7847893
Muhammad Nabil Azra <nabilazra1234@gmail.com>
2025-10-10 06:38:51 +0000

    GANTI SMEUA FONT KITA JADI ALA NSANS GOOGLE : Embed code in the <head> o

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@700&family=Caveat:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Alan+Sans:wght@300..900&display=swap"
           rel="stylesheet"
         />
       </head>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
+        body: ['"Alan Sans"', 'sans-serif'],
+        headline: ['"Alan Sans"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
