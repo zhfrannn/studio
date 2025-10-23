83cad743c69f874467e40b59ef79025804f9b277
Muhammad Hafizh Zhafran <hazhranz@gmail.com>
2025-10-23 21:54:54 +0000

    Try fixing this error: `Runtime ReferenceError: React is not defined. Er

--- src/app/share-story/page.tsx
+++ src/app/share-story/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
